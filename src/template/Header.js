2e41b1cad5f9f966d0e6e568271939d7c442ccb0
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2020-03-18 20:37:32 -0600

    Initial commit

---
+++ src/template/Header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+    const view = `
+        <div class="header-main">
+            <div class="header__logo>
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+            
+            <div class="header__nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </div>
+    `;
+    
+    return view;
+}
+
+export default Header;
