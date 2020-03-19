d6187dbb75e7dc8a1712e4441b608181353e08bb
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2020-03-19 17:44:57 -0600

    API was connected to app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () =>
+{
+    const characters = await getData();
+
     const view = `
         <div class="characters">
+            ${characters.results.map(character => `
             <article class="character__item">
-                <a href="#/1/">
-                    <img src="image" alt="name" />
-                    <h2>Name</h2>
+                <a href="#/${characters.id}/">
+                    <img src="${character.image} " alt="${characters.name} " />
+                    <h2>${character.name} </h2>
                 </a>
             </article>
+            `).join('')}
         </div>
     `;
+
+    console.log(view);
     return view;
 }
 
